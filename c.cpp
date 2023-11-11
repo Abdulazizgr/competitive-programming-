8a673526b72a73fc57a5347f7e0ef6aa414e186a
Abdulaziz <abdulazizisa579@gmail.com>
2023-11-11 16:39:40 +0300

    u

---
+++ c.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    int sum = 0;
+    cin >> x;
+    for (int i = 0; i < x; i++)
+    {
+        cin >> y;
+        if (y > 10)
+        {
+            y = y - 10;
+            sum += y;
+        }
+    }
+    cout << sum;
+}
+
+// A - Find Multiple
