c60db197cf4b0b23d4c090a0ee934f2c4384c729
Abdulaziz <abdulazizisa579@gmail.com>
2023-11-11 20:58:20 +0300

    up

---
+++ d.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    vector<int> y(x);
+    vector<int> an;
+    int z = x;
+    for (int i = 0; i < x; i++)
+    {
+        cin >> y[i];
+        if (z > 0)
+        {
+            an.push_back(z);
+            z = z - 1;
+        }
+    }
+    sort(y.begin(), y.end(), greater<int>());
+
+    if (y == an)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+
+    return 0;
+}
